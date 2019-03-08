103617dc76e14a1158067e90722a250cc49042d0
guoshuai <1072851416@qq.com>
2019-03-08 21:29:53 +0800

    jsdkahfldk

---
+++ app/src/main/java/com/example/administrator/sportsscores/ApplyImm.java
@@ -0,0 +1,13 @@
+package com.example.administrator.sportsscores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApplyImm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_imm);
+    }
+}
